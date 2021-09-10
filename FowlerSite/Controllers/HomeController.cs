eac43af3595d624f23c933077613e517b0b12a66
Dominic Perrine <16063254@mstc.edu>
2021-09-10 17:24:22 -0500

    Fixed some of the links to use correct controller

--- FowlerSite/Controllers/HomeController.cs
+++ FowlerSite/Controllers/HomeController.cs
@@ -33,30 +33,8 @@ namespace FowlerSite.Controllers
             return View();
         }
 
-        public IActionResult Store()
-        {
-            return View("~/Views/Store/Store.cshtml");
-        }
 
-        public IActionResult StoreCart()
-        {
-            return View("~/Views/Store/StoreCart.cshtml");
-        }
 
-        public IActionResult StoreCheckout()
-        {
-            return View("~/Views/Store/StoreCheckout.cshtml");
-        }
-
-        public IActionResult StoreProduct()
-        {
-            return View("~/Views/Store/StoreProduct.cshtml");
-        }
-
-        public IActionResult StoreCatalog()
-        {
-            return View("~/Views/Store/StoreCatalog.cshtml");
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
