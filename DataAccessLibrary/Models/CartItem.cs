62730292f1561037b876ae81a1f7528f109b0880
14caadams <84451416+14caadams@users.noreply.github.com>
2021-09-29 18:29:30 -0500

    TF_15added all of the tables without relationships and added a user scaffolding for MVC

--- DataAccessLibrary/Models/CartItem.cs
+++ DataAccessLibrary/Models/CartItem.cs
@@ -8,21 +8,42 @@ namespace DataAccessLibrary.Models
 {
     public class CartItem
     {
+        /// <summary>
+        /// Gets or sets the ItemID.
+        /// </summary>
         [Key]
         public string ItemId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the CartId.
+        /// </summary>
         [ForeignKey(nameof(Cart))]
         public int CartId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the quantity of items in the cart.
+        /// </summary>
         public int Quantity { get; set; }
 
+        /// <summary>
+        /// Gets or sets the date created of the item.
+        /// </summary>
         public System.DateTime DateCreated { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Product ID of the item.
+        /// </summary>
         [ForeignKey(nameof(Game))]
         public int ProductId { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Game in the cart.
+        /// </summary>
         public virtual Game Game { get; set; }
 
+        /// <summary>
+        /// Gets or sets the cart that is being used.
+        /// </summary>
         public virtual Cart Cart { get; set; }
     }
 }
