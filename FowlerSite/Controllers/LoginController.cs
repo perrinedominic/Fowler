87bc02ebea0c7e13171ddc99b08876e1c5e2cbe6
Michael Voorhies <16641156@mstc.edu>
2021-09-29 18:32:00 -0500

    merging

---
+++ FowlerSite/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FowlerSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FowlerSite.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Login");
+        }
+    }
+}
---
+++ FowlerSite/Models/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FowlerSite.Models
+{
+    public class Login
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+}
