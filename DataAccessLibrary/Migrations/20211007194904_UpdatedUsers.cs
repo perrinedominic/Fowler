e0a5f7d3e67c892f597e434afd41ea5a9abde988
14caadams <84451416+14caadams@users.noreply.github.com>
2021-10-13 11:43:34 -0500

    TF-23 added a few migrations this will need to be updated later to fix some of the problems with migrating the orders table.

---
+++ DataAccessLibrary/Migrations/20211007194904_UpdatedUsers.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLibrary.Migrations
+{
+    public partial class UpdatedUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Subtotal",
+                table: "ShoppingCart",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Total",
+                table: "ShoppingCart",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Subtotal",
+                table: "ShoppingCart");
+
+            migrationBuilder.DropColumn(
+                name: "Total",
+                table: "ShoppingCart");
+        }
+    }
+}
